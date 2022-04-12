18a65a2f6e962d74dd8f4f4264fa315bfa3b402d
Anand Bagmar <abagmar@gmail.com>
2022-04-12 17:40:14 +0530

    added missing ; in karate-config.js

--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -1,7 +1,10 @@
 function fn() {
+  karate.log('In karate-config.js');
+  karate.log('env: ', karate.env);
   var env = karate.env; // get system property 'karate.env'
+  karate.log('from property: env: ', env);
   karate.configure('ssl', { trustAll: true });
-  var config = read('classpath:test_data.json')
+  var config = read('classpath:test_data.json');
   karate.configure('connectTimeout', 30000);
   karate.configure('readTimeout', 60000);
   config = karate.call('classpath:util.js', config[env]);
@@ -10,6 +13,6 @@ function fn() {
     username = "ci";
   }
   karate.log('Running test as:', username);
-  config.user = username
+  config.user = username;
   return config;
 }
