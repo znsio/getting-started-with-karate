866b80101291284da74a7fc5fbd1742cbf44f6e7
Anand Bagmar <abagmar@gmail.com>
2022-04-29 13:48:26 +0530

    updated karate-config.js

--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -4,22 +4,23 @@ function fn() {
   var env = karate.env; // get system property 'karate.env'
   karate.log('from property: env: ', env);
   karate.configure('ssl', { trustAll: true });
-  var config = read('classpath:test_data.json');
   karate.configure('connectTimeout', 30000);
   karate.configure('readTimeout', 60000);
-  config = function() {
-        // If you add Java code in utils.js, you may end up with the following error when running tests as a fatjar:
-        // org.graalvm.polyglot.PolyglotException: SyntaxError: No language for id regex found. Supported languages are: [js]
-        // See this post for reference: https://github.com/karatelabs/karate/issues/1515
-        // https://github.com/karatelabs/karate/issues/1515#issuecomment-800856613
-        // https://github.com/karatelabs/karate/issues/1515#issuecomment-808898387
-      karate.callSingle('classpath:util.js', config[env]);
-    }
+
+  var config = read('classpath:test_data.json');
+  config = config[env];
   var username = java.lang.System.getProperty('user.name').replace(".", "").toLowerCase();
   if (['anandbagmar'].indexOf(username) < 0) {
     username = "ci";
   }
   karate.log('Running test as:', username);
   config.user = username;
+
+  var loadCommonFunctions = function() {
+    return karate.callSingle('classpath:util.js', config);
+  }
+  config = loadCommonFunctions();
+
+  karate.log('Running test with config:', config);
   return config;
 }
