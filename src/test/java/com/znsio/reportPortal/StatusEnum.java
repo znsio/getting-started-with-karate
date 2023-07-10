94cda5786ee3f72b557775100f13847062a3e140
Shivani Singh <shivanisingh8990@gmail.com>
2023-07-10 12:54:58 +0530

    Enum class used in RPReporter

---
+++ src/test/java/com/znsio/reportPortal/StatusEnum.java
@@ -0,0 +1,11 @@
+package com.znsio.reportPortal;
+
+public class StatusEnum {
+    public static final String SKIPPED = "SKIPPED";
+    public static final String FAILED = "FAILED";
+    public static final String PASSED = "PASSED";
+    public static final String ERROR_LEVEL = "ERROR";
+    public static final String INFO_LEVEL = "INFO";
+    public static final String TEST_TYPE = "TEST";
+    public static final String STEP_TYPE = "STEP";
+}
