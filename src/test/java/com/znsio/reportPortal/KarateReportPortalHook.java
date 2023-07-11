fe6ab37cc6f3b18195a6cf2e8cb107c017fd41f0
Shivani Singh <shivanisingh8990@gmail.com>
2023-07-11 11:28:31 +0530

    Added relevant comments

--- src/test/java/com/znsio/reportPortal/KarateReportPortalHook.java
+++ src/test/java/com/znsio/reportPortal/KarateReportPortalHook.java
@@ -9,37 +9,30 @@ import com.intuit.karate.core.StepResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class KarateReportPortalHook implements RuntimeHook
-{
+public class KarateReportPortalHook implements RuntimeHook {
+    // Overrides karate before/after methods to send results on report portal
     private RPReporter rpReporter;
     private static final Logger logger = LoggerFactory.getLogger(KarateReportPortalHook.class);
 
-    public KarateReportPortalHook()
-    {
+    public KarateReportPortalHook() {
         this.rpReporter = new RPReporter();
     }
 
     @Override
-    public boolean beforeScenario(ScenarioRuntime sr)
-    {
+    public boolean beforeScenario(ScenarioRuntime sr) {
         return !this.rpReporter.isTemplate(sr.scenario);
     }
 
     @Override
-    public void afterScenario(ScenarioRuntime sr)
-    {
+    public void afterScenario(ScenarioRuntime sr) {
 
     }
 
     @Override
-    public boolean beforeFeature(FeatureRuntime fr)
-    {
-        try
-        {
-                this.rpReporter.startFeature(fr.feature);
-        }
-        catch (Exception e)
-        {
+    public boolean beforeFeature(FeatureRuntime fr) {
+        try {
+            this.rpReporter.startFeature(fr.feature);
+        } catch (Exception e) {
             logger.error("beforeFeature exception: {}", e.getMessage(), e);
         }
 
@@ -47,53 +40,39 @@ public class KarateReportPortalHook implements RuntimeHook
     }
 
     @Override
-    public void afterFeature(FeatureRuntime fr)
-    {
-        try
-        {
-            if(!this.rpReporter.isTemplate(fr.feature))
+    public void afterFeature(FeatureRuntime fr) {
+        try {
+            if (!this.rpReporter.isTemplate(fr.feature))
                 this.rpReporter.finishFeature(fr.result);
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             logger.error("afterFeature exception: {}", e.getMessage(), e);
         }
     }
 
     @Override
-    public void beforeSuite(Suite suite)
-    {
-        try
-        {
+    public void beforeSuite(Suite suite) {
+        try {
             this.rpReporter.startLaunch();
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             logger.error("beforeSuite exception: {}", e.getMessage(), e);
         }
     }
 
     @Override
-    public void afterSuite(Suite suite)
-    {
-        try
-        {
+    public void afterSuite(Suite suite) {
+        try {
             this.rpReporter.finishLaunch(suite);
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             logger.error("afterSuite exception: {}", e.getMessage(), e);
         }
     }
 
     @Override
-    public boolean beforeStep(Step step, ScenarioRuntime sr)
-    {
+    public boolean beforeStep(Step step, ScenarioRuntime sr) {
         return true;
     }
 
     @Override
-    public void afterStep(StepResult result, ScenarioRuntime sr)
-    {
+    public void afterStep(StepResult result, ScenarioRuntime sr) {
     }
 }
