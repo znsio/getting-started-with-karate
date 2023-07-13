9bb9c9438c20841aff2e5326fd5f48a30d6f37c7
Vineet Joshi <Vineet.Joshi@ril.com>
2023-07-13 11:45:38 +0530

    updated code , to remove redundant scenario and feature calls

--- src/test/java/com/znsio/reportPortal/KarateReportPortalHook.java
+++ src/test/java/com/znsio/reportPortal/KarateReportPortalHook.java
@@ -23,6 +23,7 @@ public class KarateReportPortalHook implements RuntimeHook {
     HashMap <String,Maybe<String>> featureIdentifier=new HashMap<>();
     HashMap <String,Maybe<String>> scenarioIdentifier=new HashMap<>();
     int bfSc=0;
+    int temp=0;
     List<String> scenariosA=new ArrayList<>();
     public KarateReportPortalHook() {
         this.rpReporter = new RPReporter();
@@ -42,7 +43,7 @@ public class KarateReportPortalHook implements RuntimeHook {
 
         try {
             this.rpReporter.startFeature(fr.feature);
-            if (!this.rpReporter.isTemplate(fr.feature)) {
+            if (!this.rpReporter.isTemplate(fr.feature) && !this.rpReporter.isRandomizer(fr.feature)) {
                 Maybe<String> featureId = this.rpReporter.launchFeatureToReportPortal(fr.result);
                 if(featureId!=null)
                     featureIdentifier.put(fr.feature.getName(),featureId);
@@ -56,13 +57,8 @@ public class KarateReportPortalHook implements RuntimeHook {
     @Override
     public boolean beforeScenario(ScenarioRuntime sr) {
 
-        bfSc++;
         scenariosA.add(sr.scenario.toString());
-//        if(this.rpReporter.isTemplate(sr.scenario))
-//            return true;
-//        if(this.rpReporter.isScenarioTemplate(sr.scenario))
-//            return true; //don't want to log template as a Scenario
-        if(featureIdentifier.containsKey(sr.scenario.getFeature().getName()) && featureIdentifier.get(sr.scenario.getFeature().getName())!=null) {
+        if(featureIdentifier.get(sr.scenario.getFeature().getName())!=null && !this.rpReporter.isScenarioTemplate(sr.result) ) {
             Maybe<String> scenarioId=this.rpReporter.launchScenarioToReportPortal(sr.result, featureIdentifier.get(sr.scenario.getFeature().getName()));;
             if(scenarioId!=null)
                 scenarioIdentifier.put(sr.scenario.getUniqueId(),scenarioId);
@@ -77,24 +73,28 @@ public class KarateReportPortalHook implements RuntimeHook {
 
     @Override
     public void afterStep(StepResult result, ScenarioRuntime sr) {
+        bfSc++;
         if(scenarioIdentifier.containsKey(sr.scenario.getUniqueId()))
             this.rpReporter.writeStepToReportPortal(result,sr.result,scenarioIdentifier.get(sr.scenario.getUniqueId()));
+        else if(this.rpReporter.isScenarioTemplate(sr.result))
+            temp++;
+
     }
 
     @Override
     public void afterScenario(ScenarioRuntime sr) {
-//        if(!this.rpReporter.isTemplate(sr.scenario))
-            if(scenarioIdentifier.containsKey(sr.scenario.getUniqueId()))
-                this.rpReporter.finishScenarioInReportPortal(sr.result,scenarioIdentifier.get(sr.scenario.getUniqueId()));
+            if(scenarioIdentifier.containsKey(sr.scenario.getUniqueId())) {
+                this.rpReporter.finishScenarioInReportPortal(sr.result, scenarioIdentifier.get(sr.scenario.getUniqueId()));
+                scenarioIdentifier.remove(sr.scenario.getUniqueId());
+            }
     }
-
-
 
     @Override
     public void afterFeature(FeatureRuntime fr) {
         try {
             if (!this.rpReporter.isTemplate(fr.feature) && featureIdentifier.get(fr.feature.getName())!=null) {
                 this.rpReporter.finishFeature(fr.result,featureIdentifier.get(fr.feature.getName()));
+                featureIdentifier.remove(fr.feature.getName());
             }
         } catch (Exception e) {
             logger.error("afterFeature exception: {}", e.getMessage(), e);
@@ -109,6 +109,7 @@ public class KarateReportPortalHook implements RuntimeHook {
             this.rpReporter.finishLaunch(suite);
             System.out.println("After suite bfsc "+bfSc);
             System.out.println("After suite bfsc "+scenariosA);
+            System.out.println("After suite temp "+temp);
         } catch (Exception e) {
             logger.error("afterSuite exception: {}", e.getMessage(), e);
         }
