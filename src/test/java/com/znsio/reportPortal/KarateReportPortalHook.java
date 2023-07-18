11ea230c52868fcb95bdd83771b7d43fb5ea4cba
Vineet Joshi <Vineet.Joshi@ril.com>
2023-07-18 12:02:19 +0530

    removed commented lines and other unnecessary print statements

--- src/test/java/com/znsio/reportPortal/KarateReportPortalHook.java
+++ src/test/java/com/znsio/reportPortal/KarateReportPortalHook.java
@@ -20,9 +20,6 @@ public class KarateReportPortalHook implements RuntimeHook {
     private static final Logger logger = LoggerFactory.getLogger(KarateReportPortalHook.class);
     HashMap<String, Maybe<String>> featureIdentifier = new HashMap<>();
     HashMap<String, Maybe<String>> scenarioIdentifier = new HashMap<>();
-    int bfSc = 0;
-    int temp = 0;
-    List<String> scenariosA = new ArrayList<>();
 
     public KarateReportPortalHook() {
         this.rpReporter = new RPReporter();
@@ -39,13 +36,13 @@ public class KarateReportPortalHook implements RuntimeHook {
 
     @Override
     public boolean beforeFeature(FeatureRuntime fr) {
-
+        //ignoring randomizer feauter which is being called in karate-config.js
+        //starting feature in RP
         try {
             this.rpReporter.startFeature(fr.feature);
             if (!this.rpReporter.isTemplate(fr.feature) && !this.rpReporter.isRandomizer(fr.feature)) {
                 Maybe<String> featureId = this.rpReporter.launchFeatureToReportPortal(fr.result);
-                if (featureId != null)
-                    featureIdentifier.put(fr.feature.getName(), featureId);
+                if (featureId != null) featureIdentifier.put(fr.feature.getName(), featureId);
             }
         } catch (Exception e) {
             logger.error("beforeFeature exception: {}", e.getMessage(), e);
@@ -56,12 +53,11 @@ public class KarateReportPortalHook implements RuntimeHook {
 
     @Override
     public boolean beforeScenario(ScenarioRuntime sr) {
-
-        scenariosA.add(sr.scenario.toString());
+        //starting scenario inside a feature using its feature id
+        //ignoring template to be logged as sceanrio inside a feature
         if (featureIdentifier.get(sr.scenario.getFeature().getName()) != null && !this.rpReporter.isScenarioTemplate(sr.result)) {
             Maybe<String> scenarioId = this.rpReporter.launchScenarioToReportPortal(sr.result, featureIdentifier.get(sr.scenario.getFeature().getName()));
-            if (scenarioId != null)
-                scenarioIdentifier.put(sr.scenario.getUniqueId(), scenarioId);
+            if (scenarioId != null) scenarioIdentifier.put(sr.scenario.getUniqueId(), scenarioId);
         }
         return true;
     }
@@ -73,17 +69,16 @@ public class KarateReportPortalHook implements RuntimeHook {
 
     @Override
     public void afterStep(StepResult result, ScenarioRuntime sr) {
-        bfSc++;
-
+        //loggin steps to RP
+        //storing and loggin template calls made by a step in RP
         if (scenarioIdentifier.containsKey(sr.scenario.getUniqueId()) || this.rpReporter.isScenarioTemplate(sr.result))
             this.rpReporter.writeStepToReportPortal(result, sr.result, scenarioIdentifier.get(sr.scenario.getUniqueId()));
-        else if (this.rpReporter.isScenarioTemplate(sr.result))
-            temp++;
-
     }
 
     @Override
     public void afterScenario(ScenarioRuntime sr) {
+        //finishing a Scenario in RP
+        //removing the sceanrio id from scenarioIdentifier map
         if (scenarioIdentifier.containsKey(sr.scenario.getUniqueId())) {
             this.rpReporter.finishScenarioInReportPortal(sr.result, scenarioIdentifier.get(sr.scenario.getUniqueId()));
             scenarioIdentifier.remove(sr.scenario.getUniqueId());
@@ -92,6 +87,8 @@ public class KarateReportPortalHook implements RuntimeHook {
 
     @Override
     public void afterFeature(FeatureRuntime fr) {
+        //finishing a Feature in RP
+        //removing the featureIdentifier id from featureIdentifier map
         try {
             if (!this.rpReporter.isTemplate(fr.feature) && featureIdentifier.get(fr.feature.getName()) != null) {
                 this.rpReporter.finishFeature(fr.result, featureIdentifier.get(fr.feature.getName()));
@@ -107,9 +104,6 @@ public class KarateReportPortalHook implements RuntimeHook {
     public void afterSuite(Suite suite) {
         try {
             this.rpReporter.finishLaunch(suite);
-            System.out.println("After suite bfsc " + bfSc);
-            System.out.println("After suite bfsc " + scenariosA);
-            System.out.println("After suite temp " + temp);
         } catch (Exception e) {
             logger.error("afterSuite exception: {}", e.getMessage(), e);
         }
