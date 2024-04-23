65a4a5ace0fb91b93eae1ac655b2bb5ca264e699
Anand Bagmar <abagmar@gmail.com>
2024-04-23 16:52:07 +0530

    use kendo as a dependency to setup and run the Karate tests. Updated config.properties

--- src/main/java/com/znsio/apiteswiz/exceptions/TestExecutionException.java
+++
@@ -1,7 +0,0 @@
-package com.znsio.apiteswiz.exceptions;
-
-public class TestExecutionException extends RuntimeException {
-    public TestExecutionException(String failureMessage) {
-        super(failureMessage);
-    }
-}
